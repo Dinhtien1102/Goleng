f58b632f45174df7976b55c670d02332a8b3f3b0
Dinhtien1102 <dinhconganhictuk13e1@gmail.com>
2019-07-23 19:13:48 +0700

    Style video

--- Goleng_JS.js
+++ Goleng_JS.js
@@ -1,52 +1,7 @@
-a= data.bai_1['ten_bai']
-b= data.bai_1['muc_luc']
-c= data.bai_1['content']
-d= data.bai_1['bai_khac']
-function content(){
-    // document.getElementById("banner_content").innerHTML= a
-    // document.getElementById("left_content").innerHTML=  b 
-    // document.getElementById("main_content").innerHTML = c
-    // document.getElementById("right_content").innerHTML= d 
-}
-
-// console.log(data.bai_1.ten_bai, data.bai_1.muc_luc, data.bai_1.content, data.bai_1.bai_khac,)
-// console.log(data.bai_1['muc_luc'])
-// console.log(data.bai_1.content)
-// console.log(data.bai_1.bai_khac)
-// console.log(data.bai_1.bai_khac[0])
-// document.getElementById("helo").innerHTML= data.bai_1.ten_bai
-// document.getElementById("concac").innerHTML=data.bai_2.bai_khac[2]
-// document.getElementById("vd3").innerHTML=data.bai_2['bai_khac']
-// console.log(data.bai_1['content'])
-// console.log(data.bai_1['ten_bai'])
-
-
-/*
-    key = {ten_bai, muc_luc}
-
-    co the key = 'ten_bai'
-    data.bai_1[key]
-
-    - Co 2 cach goi den con trong OBJECT
-    data.bai_1['muc_luc'] <=> data.bai_1.muc_luc
-*/
-
-// Van Tuan
-// 1 - noidung['bai1']
-// 2 - Function nhan tham so
-bai1=noidung['bai1']
-bai2=noidung['bai2']
-bai3=noidung['bai3']
-function tuan(bai1,bai2,bai3) {
-    console.log(bai1)
-    console.log(bai2)
-    console.log(bai3)
-}
-
-function tuan1() {
-    console.log(noidung.bai2)
-}
-
-function tuan2() {
-    console.log(noidung.bai3)
+function content(post){
+    document.getElementById('title_banner').innerHTML=noidung[post].title_banner
+    document.getElementById('note_banner').innerHTML=noidung[post].note_banner
+    document.getElementById('left_content').innerHTML=noidung[post].list_left
+    document.getElementById('main_content').innerHTML=noidung[post].main_content
+    document.getElementById('right_content').innerHTML=noidung[post].list_right
 }
---
+++ dataGoleng.js
@@ -0,0 +1,240 @@
+var noidung = {
+    post_0: {
+        title_banner: "000000aaaaaaaaaaaaaaa",
+        note_banner: "0000000bbbbbbbbbbbbbbbbbb",
+        list_left: "cccccccccccccccccccc",
+        main_content: "00000000000ddddddddddddddddddd",
+        list_right:  "000000000000000eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_1: {
+        title_banner: "1a1aaaaaaaaaaaaaa",
+        note_banner: "1111111bbbbbbbbbbbbbbbbbb",
+        list_left: "111111111cccccccccccccccccccc",
+        main_content: "111111111ddddddddddddddddddd",
+        list_right:  "1111111111eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_2: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_3: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_4: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_5: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_6: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_7: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_8: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_9: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_10: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_11: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_12: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_13: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_14: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_15: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_16: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_17: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_18: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_19: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_20: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_21: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_22: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_23: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_24: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_25: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_26: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_27: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_28: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_29: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_30: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_31: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_32: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    },
+    post_33: {
+        title_banner: "22222222aaaaaaaaaaaaaaa",
+        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
+        list_left: "222222222cccccccccccccccccccc",
+        main_content: "2222222222ddddddddddddddddddd",
+        list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
+    }
+}
