65fdf30763b37020c15f757dd2b7de1f7684d275
Dinhtien1102 <dinhconganhictuk13e1@gmail.com>
2019-08-24 15:24:45 +0700

    big_update

--- Gola40_JS.js
+++ Gola40_JS.js
@@ -1,6 +1,4 @@
 function content(post){
-    document.getElementById('title_banner').innerHTML=noidung[post].title_banner
-    document.getElementById('note_banner').innerHTML=noidung[post].note_banner
     document.getElementById('left_content').innerHTML=noidung[post].list_left
     document.getElementById('main_content').innerHTML=noidung[post].main_content
     document.getElementById('right_content').innerHTML=noidung[post].list_right
@@ -10,23 +8,41 @@ function menuColor(a) {
     document.getElementsByClassName("parent1_menu")[a].style.background= "#fff";
 }
 
-function menuMobi(b) {
-    if (document.getElementById(b).style.display == "") {
-        document.getElementById(b).style.display = "block"
-    }
-
-    else if (document.getElementById(b).style.display == "none") {
-        document.getElementById(b).style.display = "block"
-    }
-
-    else if (document.getElementById(b).style.display == "block") {
-        document.getElementById(b).style.display = "none"
-    }
-}
-
 function menuContent(c) {
-var list = document.getElementById("left_content");
-list.getElementsByTagName("a")[c].style.background= "#ffc40a";
+var list = document.getElementById("list_right");
+list.getElementsByTagName("a")[c].style.background= "#07c6ff";
 list.getElementsByTagName("a")[c].style.color = "#202020";
 list.getElementsByTagName("a")[c].style.fontWeight = "700"
+}
+
+// window.addEventListener('click', function(event){
+// 	var box = document.getElementById('list_4');
+// 	if (event.target != box && event.target.parentNode != box){
+//         box.style.display = 'none';
+//     }
+// });
+
+  //  click menu list 2
+window.addEventListener('click', function(e){   
+    if (document.getElementById('list_2').contains(e.target)){
+      // Clicked in box
+      document.getElementById('menu_list2').style.display = "block"
+    } else{
+      // Clicked outside the box
+      document.getElementById('menu_list2').style.display = "none"
+    }
+  });
+
+function menuMobi(b) {
+  if (document.getElementById(b).style.display == "") {
+      document.getElementById(b).style.display = "block"
+  }
+
+  else if (document.getElementById(b).style.display == "none") {
+      document.getElementById(b).style.display = "block"
+  }
+
+  else if (document.getElementById(b).style.display == "block") {
+      document.getElementById(b).style.display = "none"
+  }
 }
--- dataGola40.js
+++ dataGola40.js
@@ -1,238 +1,170 @@
 var noidung = {
     post_0: {
-        title_banner: "000000aaaaaaaaaaaaaaa",
-        note_banner: "0000000bbbbbbbbbbbbbbbbbb",
         list_left: "<div class='list_content'><ul class='list_content'><li><a href=''>D/S Bảng Phiên Âm</a></li> <li><a href=''>Danh Sách Ngữ Pháp</a></li><li><a href=''>D/S Hiện Tại Đơn</a></li></ul></div>",
         main_content: "<h1>A guide to apps and the App</h1> Directorydfgdfggggggggggggggggggggggggggggggggggggggggggggg<hr>ggggggggggggggggggggggggggggggggggggggggggdgfjngdhdfhgdfgfdgdfgdfgdfg",
         list_right:  "000000000000000eeeeeeee"
     },
     post_1: {
-        title_banner: "1a1aaaaaaaaaaaaaa",
-        note_banner: "1111111bbbbbbbbbbbbbbbbbb",
         list_left: "111111111cccccccccccccccccccc",
         main_content: "111111111ddddddddddddddddddd",
         list_right:  "1111111111eeeeeeeeeeeeeeeeeeeeee"
     },
     post_2: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_3: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_4: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_5: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_6: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_7: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_8: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_9: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_10: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_11: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_12: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_13: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_14: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_15: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_16: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_17: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_18: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_19: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_20: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_21: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_22: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_23: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_24: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_25: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_26: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_27: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_28: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_29: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_30: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_31: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_32: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
     },
     post_33: {
-        title_banner: "22222222aaaaaaaaaaaaaaa",
-        note_banner: "2222222bbbbbbbbbbbbbbbbbb",
         list_left: "222222222cccccccccccccccccccc",
         main_content: "2222222222ddddddddddddddddddd",
         list_right:  "2222222222222eeeeeeeeeeeeeeeeeeeeee"
